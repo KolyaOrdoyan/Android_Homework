ba5de9056e3704a1b26c1c685ed31d43e483eea6
KolyaOrdoyan <110338581+KolyaOrdoyan@users.noreply.github.com>
2022-10-13 12:38:04 +0400

    allProjects start project choose type for list

---
+++ All_Android_Projects/app/src/main/java/com/example/all_android_projects/sharepage/ChooseTypeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.all_android_projects.sharepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.all_android_projects.R
+
+class ChooseTypeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chusse_type)
+    }
+}
