51c16287a60d5e71a41111d796e9ab27f874401c
KolyaOrdoyan <110338581+KolyaOrdoyan@users.noreply.github.com>
2022-11-03 18:26:27 +0400

    allProjects end top charts page

--- All_Android_Projects/app/src/main/java/com/example/all_android_projects/store/fragmentgame/TopChartGameFragment.kt
+++ All_Android_Projects/app/src/main/java/com/example/all_android_projects/store/fragmentgame/TopChartGameFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.all_android_projects.R
 import com.example.all_android_projects.store.adapter.TopChartsGameAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.buttom_sheet.view.*
 import kotlinx.android.synthetic.main.fragment_top_charts.view.*
 import kotlinx.android.synthetic.main.fragment_top_charts.view.top_charts_top_free_button
 import kotlinx.android.synthetic.main.fragment_top_charts_game.view.*
@@ -31,19 +33,40 @@ class TopChartGameFragment : Fragment() {
 
         val topFreeButton = view.top_charts_top_free_button
         topFreeButton.setOnClickListener {
-            val vi: View = layoutInflater.inflate(R.layout.buttom_sheet, null)
-            val dialog = context?.let { it1 -> BottomSheetDialog(it1) }
-            dialog?.setContentView(vi)
-            dialog?.show()
+
+            val topFreeButton = view.top_charts_top_free_button
+            topFreeButton.setOnClickListener {
+
+                val buttonSheet: View = layoutInflater.inflate(R.layout.buttom_sheet, null)
+                val dialog = context?.let { it1 -> BottomSheetDialog(it1) }
+                dialog?.setContentView(buttonSheet)
+                dialog?.show()
+                val topFree = buttonSheet.button_sheet_top_free
+                topFree.setOnClickListener {
+                    alertDialog("This is top free apps")
+                }
+                val topGrossing = buttonSheet.button_sheet_top_grossing
+                topGrossing.setOnClickListener {
+                    alertDialog("This is top grossing apps")
+                }
+                val topPaid = buttonSheet.button_sheet_top_paid
+                topPaid.setOnClickListener {
+                    alertDialog("This is top paid apps")
+                }
+            }
+
         }
 
-//        val topFree: TextView = view.findViewById(R.id.button_sheet_top_free)
-//        topFree.setOnClickListener {
-//            topFree.text = "you pressed the button"
-//        }
     }
 
     companion object {
         fun newInstance() = TopChartGameFragment()
+    }
+
+    private fun alertDialog(message: String) {
+        val builder = context?.let { it2 -> AlertDialog.Builder(it2) }
+        builder?.setTitle("Question")
+        val message = builder?.setMessage(message)
+        builder?.show()
     }
 }
